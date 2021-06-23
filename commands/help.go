564acee407e310cdc1734de33732075f634e3ec4
WantedMurder <ecwrob@gmail.com>
2021-06-23 02:12:14 +0200

    Moderation

--- commands/help.go
+++ commands/help.go
@@ -10,8 +10,8 @@ import (
 
 func (cmd *Commands) Help(s *discordgo.Session, m *discordgo.Message, ctx *Context) {
 	if len(ctx.Fields) == 0 {
-		defaultHelp.Title = fmt.Sprintf("%s anti-nuke", s.State.User.Username)
-		defaultHelp.Description = fmt.Sprintf("Below are all the commands categories, type `%shelp [category]` to view all commands in the category.", ctx.Prefix)
+		defaultHelp.Title = fmt.Sprintf("⚙️%s Anti Wizz⚙️", s.State.User.Username)
+		defaultHelp.Description = fmt.Sprintf("type `%shelp [category]` to view all commands in the category.", ctx.Prefix)
 		defaultHelp.Footer = &discordgo.MessageEmbedFooter{Text: fmt.Sprintf("Requested by by: %s", m.Author.Username)}
 		defaultHelp.Thumbnail = &discordgo.MessageEmbedThumbnail{URL: s.State.User.AvatarURL("1024")}
 		s.ChannelMessageSendEmbed(m.ChannelID, defaultHelp)
@@ -24,19 +24,16 @@ func (cmd *Commands) Help(s *discordgo.Session, m *discordgo.Message, ctx *Conte
 	switch ctx.Fields[0] {
 
 	case "information":
-		certainHelp.Title = "Information Commands"
+		certainHelp.Title = "📁Information Commands"
 		certainHelp.Description = fmt.Sprintf("`%sserverinfo` | Returns information about the current server \n `%sbotinfo` | Shows information about the bot \n `%suserinfo [@user]` | Shows informati on about the mentioned user \n `%savatar [@user]` | Returns the mentioned users avatar \n `%smembercount` | Returns the server's member count \n `%sbanner` | Returns the server banner \n `%sservericon` | Returns the server icon", ctx.Prefix, ctx.Prefix, ctx.Prefix, ctx.Prefix, ctx.Prefix, ctx.Prefix, ctx.Prefix) //yeah ummm we don't talk about this..
-
 	case "anti":
-		certainHelp.Title = "Anti Commands"
+		certainHelp.Title = "📁Anti Commands"
 		certainHelp.Description = fmt.Sprintf("`%swhitelist [@user] (server owner)` | Whitelists the mentioned user \n `%sunwhitelist [@user] (server owner)` | Dewhitelists the mentioned user \n `%swhitelisted` | Shows whitelisted list", ctx.Prefix, ctx.Prefix, ctx.Prefix)
-
 	case "moderation":
-		certainHelp.Title = "Moderation Commands"
+		certainHelp.Title = "📁Moderation Commands"
 		certainHelp.Description = fmt.Sprintf("`%sban [@user]` | Bans the mentioned user \n `%skick [@user]` | Kicks the mentioned user \n `%spurge [amount]` | Purges entered amount of messages\n`%slock` | Locks the channel\n `%sunlock` | Unlocks the channel\n`%sslowmode [time]` | Sets the channel slowmode to that time\n`%sunslowmode` | Disables slow mode\n`%smassunban` | Unbans all members in the server", ctx.Prefix, ctx.Prefix, ctx.Prefix, ctx.Prefix, ctx.Prefix, ctx.Prefix, ctx.Prefix, ctx.Prefix)
-
 	case "settings":
-		certainHelp.Title = "Settings"
+		certainHelp.Title = "📁Settings"
 		certainHelp.Description = fmt.Sprintf("`%saddowner [@user] (server owner)` | Makes it able for someone else to use owner commands\n`%sdelowner [@user] (server owner)` | Revokes ability to use owner commands`%sprefix [prefix]` | Sets the bot prefix\n`%slogchannel (server owner)` | Sets the log channel for all notifications relating to the anti-nuke.\n`%santiinvite [on/off]` | Enables/Disables the anti invite system", ctx.Prefix, ctx.Prefix, ctx.Prefix, ctx.Prefix, ctx.Prefix)
 
 	default:
@@ -58,10 +55,10 @@ func (cmd *Commands) Setup(s *discordgo.Session, m *discordgo.Message, ctx *Cont
 	s.ChannelMessageSendEmbed(m.ChannelID, &discordgo.MessageEmbed{
 		Title: "Setup",
 		Fields: []*discordgo.MessageEmbedField{
-			{Name: "How to setup:", Value: fmt.Sprintf("%s comes ready to protect your server on join, so you don't need to do anything besides **MAKE SURE** the bot is above every other role. The bot needs to be above everyone else to be able to ban.\n", s.State.User.Username), Inline: true},
-			{Name: "Whitelist:", Value: fmt.Sprintf("To exclude someone from being banned, type `%swhitelist [@user]`. Only the server owner may whitelist and unwhitelist members.\n", ctx.Prefix)},
-			{Name: "Logs:", Value: fmt.Sprintf("With %s, you can set a channel to log whenever %s bans someone for attempting to nuke. To set the log channel, type `%slog`\n", s.State.User.Username, s.State.User.Username, ctx.Prefix)},
-			{Name: "Support:", Value: "If you need any help, click [here](https://discord.gg/list) to join the support server!"},
+			{Name: "📁How to setup:", Value: fmt.Sprintf("%s comes ready to protect your server on join, so you don't need to do anything besides **MAKE SURE** the bot is above every other role. The bot needs to be above everyone else to be able to ban.\n", s.State.User.Username), Inline: true},
+			{Name: "📁Whitelist:", Value: fmt.Sprintf("To exclude someone from being banned, type `%swhitelist [@user]`. Only the server owner may whitelist and unwhitelist members.\n", ctx.Prefix)},
+			{Name: "📁Logs:", Value: fmt.Sprintf("With %s, you can set a channel to log whenever %s bans someone for attempting to nuke. To set the log channel, type `%slog`\n", s.State.User.Username, s.State.User.Username, ctx.Prefix)},
+			{Name: "📁Support:", Value: "If you need any help, click [here](https://discord.gg/list) to join the support server!"},
 		},
 		Color: 0xFFB070,
 	})
--- commands/moderation.go
+++ commands/moderation.go
@@ -20,19 +20,19 @@ func (cmd *Commands) Ban(s *discordgo.Session, m *discordgo.Message, ctx *Contex
 	)
 
 	if !utils.IsAbove(userRole, targetRole) && m.Author.ID != utils.GetGuildOwner(s, m.GuildID) {
-		s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("You do not have the proper permissions to ban <@%s>", m.Mentions[0].ID))
+		s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("<:X_Mark:857046727530250271> | You do not have the proper permissions to ban <@%s>", m.Mentions[0].ID))
 		return
 	}
 
 	err = s.GuildBanCreateWithReason(m.GuildID, m.Mentions[0].ID, fmt.Sprintf("%s | Command ban", s.State.User.Username), 0)
 
 	if err != nil {
-		s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Could not ban <@%s>", m.Mentions[0].ID))
+		s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("<:X_Mark:857046727530250271> | Could not ban <@%s>", m.Mentions[0].ID))
 		return
 	}
 
 	s.ChannelMessageSendEmbed(m.ChannelID, &discordgo.MessageEmbed{
-		Description: fmt.Sprintf("Successfully banned <@%s> from the server.", m.Mentions[0].ID),
+		Description: fmt.Sprintf("<:V_Mark:857046695577518090> | Successfully banned <@%s> from the server.", m.Mentions[0].ID),
 		Footer:      &discordgo.MessageEmbedFooter{Text: fmt.Sprintf("banned by: %s", m.Author.Username)},
 		Color:       0x36393F,
 	})
@@ -50,18 +50,18 @@ func (cmd *Commands) Kick(s *discordgo.Session, m *discordgo.Message, ctx *Conte
 	)
 
 	if !utils.IsAbove(userRole, targetRole) && m.Author.ID != utils.GetGuildOwner(s, m.GuildID) {
-		s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("You do not have the proper permissions to kick <@%s>", m.Mentions[0].ID))
+		s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("<:X_Mark:857046727530250271> | You do not have the proper permissions to kick <@%s>", m.Mentions[0].ID))
 		return
 	}
 
 	err = s.GuildMemberDeleteWithReason(m.GuildID, m.Mentions[0].ID, fmt.Sprintf("%s | Command kick", s.State.User.Username))
 
 	if err != nil {
-		s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Could not kick <@%s>", m.Mentions[0].ID))
+		s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("<:X_Mark:857046727530250271> | Could not kick <@%s>", m.Mentions[0].ID))
 		return
 	}
 	s.ChannelMessageSendEmbed(m.ChannelID, &discordgo.MessageEmbed{
-		Description: fmt.Sprintf("Successfully kicked <@%s> from the server.", m.Mentions[0].ID),
+		Description: fmt.Sprintf("<:V_Mark:857046695577518090> | Successfully kicked <@%s> from the server.", m.Mentions[0].ID),
 		Footer:      &discordgo.MessageEmbedFooter{Text: fmt.Sprintf("Kicked by: %s", m.Author.Username)},
 		Color:       0x36393F,
 	})
@@ -71,11 +71,11 @@ func (cmd *Commands) Lockdown(s *discordgo.Session, m *discordgo.Message, ctx *C
 
 	err := s.ChannelPermissionSet(m.ChannelID, m.GuildID, "role", 0, discordgo.PermissionSendMessages)
 	if err != nil {
-		s.ChannelMessageSend(m.ChannelID, "Couldn't lock this channel. Maybe check perms?")
+		s.ChannelMessageSend(m.ChannelID, "<:X_Mark:857046727530250271> | Couldn't lock this channel. Maybe check perms?")
 		return
 	}
 	s.ChannelMessageSendEmbed(m.ChannelID, &discordgo.MessageEmbed{
-		Description: fmt.Sprintf("Successfully locked <#%s>.", m.ChannelID),
+		Description: fmt.Sprintf("<:V_Mark:857046695577518090> | Successfully locked <#%s>.", m.ChannelID),
 		Footer:      &discordgo.MessageEmbedFooter{Text: fmt.Sprintf("Locked by: %s", m.Author.Username)},
 		Color:       0x36393F,
 	})
@@ -84,14 +84,14 @@ func (cmd *Commands) Lockdown(s *discordgo.Session, m *discordgo.Message, ctx *C
 func (cmd *Commands) Purge(s *discordgo.Session, m *discordgo.Message, ctx *Context) {
 	amount, err := strconv.Atoi(ctx.Fields[0])
 	if err != nil || amount > 100 {
-		s.ChannelMessageSend(m.ChannelID, "You have to specify a number and it has to be inbetween 1-100")
+		s.ChannelMessageSend(m.ChannelID, "<:X_Mark:857046727530250271> | You have to specify a number and it has to be inbetween 1-100")
 		return
 	}
 	var tempMessages []string
 
 	messages, err := s.ChannelMessages(m.ChannelID, amount, m.ID, "", "")
 	if err != nil {
-		s.ChannelMessageSend(m.ChannelID, "Could not fetch channel messages")
+		s.ChannelMessageSend(m.ChannelID, "<:X_Mark:857046727530250271> | Could not fetch channel messages")
 		return
 	}
 
@@ -118,11 +118,11 @@ func (cmd *Commands) SlowMode(s *discordgo.Session, m *discordgo.Message, ctx *C
 	})
 
 	if err != nil {
-		s.ChannelMessageSend(m.ChannelID, "Couldn't set slowmode on this channel. Maybe check perms?")
+		s.ChannelMessageSend(m.ChannelID, "<:X_Mark:857046727530250271> | Couldn't set slowmode on this channel. Maybe check perms?")
 		return
 	}
 	s.ChannelMessageSendEmbed(m.ChannelID, &discordgo.MessageEmbed{
-		Description: fmt.Sprintf("Set <#%s> slowmode to %d seconds", channel.ID, seconds),
+		Description: fmt.Sprintf("<:V_Mark:857046695577518090> | Set <#%s> slowmode to %d seconds", channel.ID, seconds),
 		Footer:      &discordgo.MessageEmbedFooter{Text: fmt.Sprintf("Slowmode set by: %s", m.Author.Username)},
 		Color:       0x36393F,
 	})
@@ -136,7 +136,7 @@ func (cmd *Commands) Unban(s *discordgo.Session, m *discordgo.Message, ctx *Cont
 
 	bans, err := s.GuildBans(m.GuildID)
 	if err != nil {
-		s.ChannelMessageSend(m.ChannelID, "Could not fetch the guild bans")
+		s.ChannelMessageSend(m.ChannelID, "<:X_Mark:857046727530250271> | Could not fetch the guild bans")
 		return
 	}
 
@@ -149,7 +149,7 @@ func (cmd *Commands) Unban(s *discordgo.Session, m *discordgo.Message, ctx *Cont
 		nukebot++
 	}
 	s.ChannelMessageSendEmbed(m.ChannelID, &discordgo.MessageEmbed{
-		Description: fmt.Sprintf("Unbanned %d members | Didn't unban %d account(s) banned by %s.", unbancount, nukebot, s.State.User.Username),
+		Description: fmt.Sprintf("<:V_Mark:857046695577518090> | Unbanned %d members | <:X_Mark:857046727530250271> | Didn't unban %d account(s) banned by %s.", unbancount, nukebot, s.State.User.Username),
 		Footer:      &discordgo.MessageEmbedFooter{Text: fmt.Sprintf("Mass-Unban started by: %s", m.Author.Username)},
 		Color:       0x36393F,
 	})
@@ -159,11 +159,11 @@ func (cmd *Commands) UnLockdown(s *discordgo.Session, m *discordgo.Message, ctx 
 
 	err := s.ChannelPermissionSet(m.ChannelID, m.GuildID, "role", discordgo.PermissionSendMessages, discordgo.PermissionMentionEveryone|discordgo.PermissionSendTTSMessages)
 	if err != nil {
-		s.ChannelMessageSend(m.ChannelID, "Couldn't unlock this channel. Maybe check perms?")
+		s.ChannelMessageSend(m.ChannelID, "<:X_Mark:857046727530250271> | Couldn't unlock this channel. Maybe check perms?")
 		return
 	}
 	s.ChannelMessageSendEmbed(m.ChannelID, &discordgo.MessageEmbed{
-		Description: fmt.Sprintf("Successfully unlocked <#%s>.", m.ChannelID),
+		Description: fmt.Sprintf("<:V_Mark:857046695577518090> | Successfully unlocked <#%s>.", m.ChannelID),
 		Footer:      &discordgo.MessageEmbedFooter{Text: fmt.Sprintf("Unlocked by: %s", m.Author.Username)},
 		Color:       0x36393F,
 	})
@@ -175,11 +175,11 @@ func (cmd *Commands) UnSlowMode(s *discordgo.Session, m *discordgo.Message, ctx 
 	})
 
 	if err != nil {
-		s.ChannelMessageSend(m.ChannelID, "Couldn't turn off slowmode on this channel. Maybe check perms?")
+		s.ChannelMessageSend(m.ChannelID, "<:X_Mark:857046727530250271> | Couldn't turn off slowmode on this channel. Maybe check perms?")
 		return
 	}
 	s.ChannelMessageSendEmbed(m.ChannelID, &discordgo.MessageEmbed{
-		Description: fmt.Sprintf("Successfully disabled slowmode for <#%s>.", channel.ID),
+		Description: fmt.Sprintf("<:V_Mark:857046695577518090> | Successfully disabled slowmode for <#%s>.", channel.ID),
 		Footer:      &discordgo.MessageEmbedFooter{Text: fmt.Sprintf("Slowmode turned off by: %s", m.Author.Username)},
 		Color:       0x36393F,
 	})
